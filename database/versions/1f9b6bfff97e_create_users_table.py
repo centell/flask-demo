8fdb211e06919c6cd52e824af42ce1e869436dc4
Centell <centell@me.com>
2019-10-16 09:39:18 +0900

    update create_users_table migration

--- database/versions/1f9b6bfff97e_create_users_table.py
+++ database/versions/1f9b6bfff97e_create_users_table.py
@@ -7,7 +7,7 @@ Create Date: 2019-10-15 16:15:58.827570
 """
 from alembic import op
 import sqlalchemy as sa
-
+import sqlalchemy_utils as su
 
 # revision identifiers, used by Alembic.
 revision = '1f9b6bfff97e'
@@ -17,8 +17,21 @@ depends_on = None
 
 
 def upgrade():
+    op.create_table(
+        'Users',
+        sa.Column('id', su.types.uuid.UUIDType, nullable=False),
+        sa.Column('username', sa.VARCHAR(length=100), nullable=False),
+        sa.Column('password', sa.CHAR(length=41), nullable=False),
+        sa.Column('name', sa.VARCHAR(length=30), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        mysql_charset='utf8mb4',
+        mysql_collate='utf8mb4_unicode_ci',
+        mysql_engine='InnoDB',
+        mysql_row_format='DYNAMIC'
+    )
     pass
 
 
 def downgrade():
+    op.drop_table('Users')
     pass
